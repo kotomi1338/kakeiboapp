67ad3f7eab69b5e717a8f65311c16c223b24f0e7
kotomi1338 <kotomin1216@gmail.com>
2019-06-29 00:22:38 +0900

    Run rails g migration AddColumnToCost date:date rails db:migrate

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_25_062922) do
+ActiveRecord::Schema.define(version: 2019_06_28_151957) do
 
   create_table "costs", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2019_06_25_062922) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
   end
 
 end
